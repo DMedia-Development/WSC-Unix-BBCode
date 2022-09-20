634c36d5a3ffb6d73d611463e66993e292813058
IceQ1337 <IceQ1337@gmx.de>
2022-09-20 16:18:02 +0200

    Add attributes to specify date/time format and icon

--- files/lib/system/bbcode/UnixTimestampBBCode.class.php
+++ files/lib/system/bbcode/UnixTimestampBBCode.class.php
@@ -23,27 +23,48 @@ class UnixTimestampBBCode extends AbstractBBCode
     {
         $value = '';
 
-        if (isset($openingTag['attributes'][0])) {
-            $timestamp = \intval($openingTag['attributes'][0]);
-
-            if (!$this->isUnixTimestamp($timestamp)) {
-                return $value;
-            }
-
-            $dateTime = DateUtil::getDateTimeByTimestamp($timestamp);
-
-            return \str_replace(
-                '%time%',
-                DateUtil::format($dateTime, 'H:i:s'),
-                \str_replace(
-                    '%date%',
-                    DateUtil::format($dateTime, 'l, j. F Y'),
-                    WCF::getLanguage()->get('wcf.date.dateTimeFormat')
-                )
-            );
+        if (!isset($openingTag['attributes'][0])) {
+            return $value;
         }
 
-        return $value;
+        $timestamp = \intval($openingTag['attributes'][0]);
+
+        if (!$this->isUnixTimestamp($timestamp)) {
+            return $value;
+        }
+
+        $dateFormat = 'l, j. F Y';
+        $timeFormat = 'H:i:s';
+
+        if (isset($openingTag['attributes'][1]) && $openingTag['attributes'][1] != 'default') {
+            $dateFormat = $openingTag['attributes'][1];
+        }
+
+        if (isset($openingTag['attributes'][2]) && $openingTag['attributes'][2] != 'default') {
+            $timeFormat = $openingTag['attributes'][2];
+        }
+
+        $dateTime = DateUtil::getDateTimeByTimestamp($timestamp);
+
+        $value = \str_replace(
+            '%time%',
+            DateUtil::format($dateTime, $timeFormat),
+            \str_replace(
+                '%date%',
+                DateUtil::format($dateTime, $dateFormat),
+                WCF::getLanguage()->get('wcf.date.dateTimeFormat')
+            )
+        );
+
+        if (!isset($openingTag['attributes'][3]) || $parser->getOutputType() == 'text/plain') {
+            return $value;
+        }
+
+        return \str_replace(
+            '%ICON_NAME%',
+            $openingTag['attributes'][3],
+            '<span class="icon %ICON_NAME%"></span> '
+        ) . $value;
     }
 
     /**
